353b6a22cd482b20523b099806ff7051f276eb8a
Luis Valdez <luis.valdez@affluences.com>
2020-01-27 17:45:58 +0100

    estimation

--- Battery/__init__.py
+++ Battery/__init__.py
@@ -3,16 +3,19 @@ import azure.functions as func
 
 
 def main(request: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
-    logging.info(f"HTTP trigger executed!")
-
-    logging.info(f"Headers: {request.headers}")
-    logging.info(f"Params: {request.params}")
-    logging.info(f"Route Params: {request.route_params}")
-    logging.info(f"Body: {request.get_body()}")
-
+    response = "le body n'a pas le format requis. {current_percentage: int (0 - 100)}"
+    speed = 1.5
     try:
-        logging.info(f"Body JSON: {request.get_json()}")
-        msg.set(f"{request.get_json()}")
+        body = request.get_json()
+
+        if body['current_percentage'] > 100 or body['current_percentage'] < 0:
+            logging.info(f"salut les amis")
+            raise ValueError
+        res = {'duration': (100 - body['current_percentage']) * speed, 'percentage': body['current_percentage']}
+        response = "le temp restant est de {}\nLa donnée a été ajoutée dans la queue".format(res['duration'])
+        msg.set(f"{body}")
     except ValueError:
+        response = f"current_percentage doit être compris entre 0 et 100"
+    except Exception:
         pass
-    return func.HttpResponse(f"Hello World!", status_code=200)
+    return func.HttpResponse(response, status_code=200)
