516f1425378f2980469beec3a4f981417546a58b
Luis Valdez <luis.valdez@affluences.com>
2020-01-29 14:24:34 +0100

    fix bus message

--- Battery/__init__.py
+++ Battery/__init__.py
@@ -13,7 +13,7 @@ def main(request: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
             raise ValueError
         res = {'duration': (100 - body['current_percentage']) * speed, 'percentage': body['current_percentage']}
         response = "le temp restant est de {}\nLa donnée a été ajoutée dans la queue".format(res['duration'])
-        msg.set(f"{body}")
+        msg.set(f"{res}")
     except ValueError:
         response = f"current_percentage doit être compris entre 0 et 100"
     except Exception:
